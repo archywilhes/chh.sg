61e42af4db94988c804e52be04b4d7afc1eb228e
Archy Wilhes <a@0a.io>
2017-11-24 04:35:01 +0800

    fixed dumb bug

--- js/industrial-fans-and-silencers.js
+++ js/industrial-fans-and-silencers.js
@@ -1,25 +1,48 @@
 var slider, slider2;
+function initSlide1(){
+  if(slider === undefined){
+  slider = $('.slider').bxSlider({
+    auto: true,
+    controls: false,
+    pager: false
+  })
+  $("#previousSlide").click(function(){
+    slider.goToPrevSlide()
+  })
+  $("#nextSlide").click(function(){
+    slider.goToNextSlide()
+  })
+}else{
+  setTimeout(function(){location.reload()},300)
+}
+}
+function initSlide2(){
+  if(slider2 === undefined){
+  slider2 = $('.slider2').bxSlider({
+    auto: true,
+    controls: false,
+    pager: false
+  })
+  $("#previousSlide2").click(function(){
+    slider2.goToPrevSlide()
+  })
+  $("#nextSlide2").click(function(){
+    slider2.goToNextSlide()
+  })
+}else{
+  setTimeout(function(){location.reload()},300)
+}
+}
 $(document).ready(function(){
-      slider = $('.slider').bxSlider({
-        auto: true,
-        controls: false,
-        pager: false
-      })
-      $("#previousSlide").click(function(){
-        slider.goToPrevSlide()
-      })
-      $("#nextSlide").click(function(){
-        slider.goToNextSlide()
-      })
-      slider2 = $('.slider2').bxSlider({
-        auto: true,
-        controls: false,
-        pager: false
-      })
-      $("#previousSlide2").click(function(){
-        slider2.goToPrevSlide()
-      })
-      $("#nextSlide2").click(function(){
-        slider2.goToNextSlide()
-      })
+  if(document.location.href.indexOf("industrial-fans-and-silencers") != -1){
+        initSlide1()
+  }else if(document.location.href.indexOf("silencers") != -1)
+        initSlide2()
    });
+
+   $("#industrial-fans-and-silencers").click(function(){
+     setTimeout(function(){initSlide1()},100)
+   })
+   $("#silencers").click(function(){
+     setTimeout(function(){initSlide2()},100)
+   })
